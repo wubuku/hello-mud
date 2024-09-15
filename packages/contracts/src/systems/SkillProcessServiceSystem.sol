ee2ad1ccff48e04d86c2bab8a9456a796cf7ebb4
yangjiefeng <yangjiefeng@gmail.com>
2024-09-16 03:08:32 +0800

    update code

--- packages/contracts/src/systems/SkillProcessServiceSystem.sol
+++ packages/contracts/src/systems/SkillProcessServiceSystem.sol
@@ -3,13 +3,13 @@ pragma solidity >=0.8.24;
 
 import { System } from "@latticexyz/world/src/System.sol";
 import { ItemIdQuantityPair } from "./ItemIdQuantityPair.sol";
+import { EnergyToken } from "../codegen/index.sol";
 import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract SkillProcessServiceSystem is System {
   using SafeERC20 for IERC20;
 
   function skillProcessServiceStartCreation(
-    IERC20 token, // TODO For testing, remove later
     uint8 skillProcessIdSkillType,
     uint256 skillProcessIdPlayerId,
     uint8 skillProcessIdSequenceNumber,
@@ -18,10 +18,12 @@ contract SkillProcessServiceSystem is System {
     uint8 itemCreationIdSkillType,
     uint32 itemCreationIdItemId
   ) public {
-    uint256 amount = 1; // For testing
-    require(address(token) != address(0), "Invalid token address");
+    uint256 amount = 1; // TODO: Only for testing
+    address tokenAddress = EnergyToken.get();
+    require(tokenAddress != address(0), "Invalid token address");
     require(amount > 0, "Amount must be greater than 0");
 
+    IERC20 token = IERC20(tokenAddress);
     // SafeERC20 处理了转账失败的情况，会自动回滚交易
     token.safeTransferFrom(_msgSender(), address(this), amount);
 
