d0ff31cc8b9f7291198dbe0ed284e032173b0012
yangjiefeng <yangjiefeng@gmail.com>
2024-09-19 23:02:18 +0800

    update RosterTransferShip logic

--- packages/contracts/src/systems/RosterTransferShipLogic.sol
+++ packages/contracts/src/systems/RosterTransferShipLogic.sol
@@ -2,15 +2,24 @@
 pragma solidity >=0.8.24;
 
 import { RosterShipTransferred } from "./RosterEvents.sol";
-import { RosterData } from "../codegen/index.sol";
+import { RosterData, ShipData, Roster, Ship } from "../codegen/index.sol";
+import { RosterDataInstance } from "../utils/RosterDataInstance.sol";
+import { ShipIdUtil } from "../utils/ShipIdUtil.sol";
+import { RosterSequenceNumber } from "./RosterSequenceNumber.sol";
 
-error ShipNotFoundInSourceRoster(uint256 shipId, uint256 rosterId);
-error RostersTooFarAway(uint256 fromRosterId, uint256 toRosterId);
-error RosterInBattle(uint256 rosterId, uint256 battleId);
-error ToRosterInBattle(uint256 toRosterId, uint256 battleId);
-error RosterShipsFull(uint256 rosterId, uint32 currentShipCount, uint32 maxShipCount);
+error ShipNotFoundInSourceRoster(uint256 shipId, uint256 rosterPlayerId, uint32 rosterSequenceNumber);
+error RostersTooFarAway(
+  uint256 rosterPlayerId,
+  uint32 rosterSequenceNumber,
+  uint256 toRosterPlayerId,
+  uint32 toRosterSequenceNumber
+);
+error RosterInBattle(uint256 rosterPlayerId, uint32 rosterSequenceNumber, uint256 battleId);
+error ToRosterInBattle(uint256 toRosterPlayerId, uint32 toRosterSequenceNumber, uint256 battleId);
 
 library RosterTransferShipLogic {
+  using RosterDataInstance for RosterData;
+
   function verify(
     uint256 playerId,
     uint32 sequenceNumber,
@@ -19,34 +28,24 @@ library RosterTransferShipLogic {
     uint32 toRosterSequenceNumber,
     uint64 toPosition,
     RosterData memory rosterData
-  ) internal pure returns (RosterShipTransferred memory) {
-    // TODO: Get or create these variables
-    RosterData memory toRoster;
-    uint64 currentTimestamp;
+  ) internal view returns (RosterShipTransferred memory) {
+    RosterData memory toRoster = Roster.get(toRosterPlayerId, toRosterSequenceNumber);
+    uint64 currentTimestamp = uint64(block.timestamp);
 
-    // TODO: Implement permission checks
-    // TODO: Check if rosters are close enough
-    // if (!areRostersCloseEnoughToTransfer(rosterData, toRoster)) {
-    //   revert RostersTooFarAway(rosterData.id, toRoster.id);
-    // }
+    if (!rosterData.areRostersCloseEnoughToTransfer(toRoster)) {
+      revert RostersTooFarAway(playerId, sequenceNumber, toRosterPlayerId, toRosterSequenceNumber);
+    }
 
-    // TODO: Check if destination roster is full (if not UNASSIGNED_SHIPS)
-    // if (toRosterSequenceNumber != UNASSIGNED_SHIPS) {
-    //   (bool isFull, uint32 currentCount, uint32 maxCount) = isRosterShipsFull(toRoster);
-    //   if (isFull) {
-    //     revert RosterShipsFull(toRoster.id, currentCount, maxCount);
-    //   }
-    // }
+    if (toRosterSequenceNumber != RosterSequenceNumber.UNASSIGNED_SHIPS) {
+      toRoster.assertRosterShipsNotFull();
+    }
 
-    // TODO: Check if rosters are in battle
-    // if (rosterData.shipBattleId != 0) {
-    //   revert RosterInBattle(rosterData.id, rosterData.shipBattleId);
-    // }
-    // if (toRoster.shipBattleId != 0) {
-    //   revert ToRosterInBattle(toRoster.id, toRoster.shipBattleId);
-    // }
-
-    // TODO: Add more checks if needed
+    if (rosterData.shipBattleId != 0) {
+      revert RosterInBattle(playerId, sequenceNumber, rosterData.shipBattleId);
+    }
+    if (toRoster.shipBattleId != 0) {
+      revert ToRosterInBattle(toRosterPlayerId, toRosterSequenceNumber, toRoster.shipBattleId);
+    }
 
     return
       RosterShipTransferred({
@@ -63,43 +62,28 @@ library RosterTransferShipLogic {
   function mutate(
     RosterShipTransferred memory rosterShipTransferred,
     RosterData memory rosterData
-  ) internal pure returns (RosterData memory) {
-    // TODO: Get or create these variables
-    RosterData memory toRoster;
+  ) internal returns (RosterData memory) {
+    RosterData memory toRoster = Roster.get(
+      rosterShipTransferred.toRosterPlayerId,
+      rosterShipTransferred.toRosterSequenceNumber
+    );
     uint256 shipId = rosterShipTransferred.shipId;
     uint64 toPosition = rosterShipTransferred.toPosition;
     uint64 transferredAt = rosterShipTransferred.transferredAt;
 
-    // TODO: Remove ship from source roster
-    // bool shipRemoved = removeShipId(rosterData.shipIds, shipId);
-    // if (!shipRemoved) {
-    //   revert ShipNotFoundInSourceRoster(shipId, rosterData.id);
-    // }
+    rosterData.shipIds = ShipIdUtil.removeShipId(rosterData.shipIds, shipId);
+    if (rosterData.shipIds.length == 0) {
+      revert ShipNotFoundInSourceRoster(shipId, rosterShipTransferred.playerId, rosterShipTransferred.sequenceNumber);
+    }
+    rosterData.speed = rosterData.calculateRosterSpeed();
 
-    // TODO: Get ship data
-    // ShipData memory ship = removeShip(rosterData.ships, shipId);
+    toRoster.shipIds = ShipIdUtil.addShipId(toRoster.shipIds, shipId, toPosition);
+    toRoster.speed = toRoster.calculateRosterSpeed();
 
-    // TODO: Recalculate speed for source roster
-    // uint32 fromSpeed = calculateRosterSpeed(rosterData);
-    // rosterData.speed = fromSpeed;
-
-    // TODO: Add ship to destination roster
-    // if (toPosition != type(uint64).max) {
-    //   insertShipId(toRoster.shipIds, shipId, toPosition);
-    // } else {
-    //   toRoster.shipIds.push(shipId);
-    // }
-    // addShip(toRoster.ships, shipId, ship);
-
-    // TODO: Recalculate speed for destination roster
-    // uint32 toSpeed = calculateRosterSpeed(toRoster);
-    // toRoster.speed = toSpeed;
-
-    // Update coordinates updated timestamp for both rosters
     rosterData.coordinatesUpdatedAt = transferredAt;
-    // toRoster.coordinatesUpdatedAt = transferredAt;
+    toRoster.coordinatesUpdatedAt = transferredAt;
 
-    // TODO: Update toRoster data
+    Roster.set(rosterShipTransferred.toRosterPlayerId, rosterShipTransferred.toRosterSequenceNumber, toRoster);
 
     return rosterData;
   }
