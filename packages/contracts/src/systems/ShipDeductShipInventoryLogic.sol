013e6eaccf21a6b160a14443a415104909ff713d
yangjiefeng <yangjiefeng@gmail.com>
2024-09-22 12:16:29 +0800

    update ShipDeductShipInventoryLogic

--- packages/contracts/src/systems/ShipDeductShipInventoryLogic.sol
+++ packages/contracts/src/systems/ShipDeductShipInventoryLogic.sol
@@ -4,6 +4,10 @@ pragma solidity >=0.8.24;
 import { ShipInventoryDeducted } from "./ShipEvents.sol";
 import { ShipData } from "../codegen/index.sol";
 import { ItemIdQuantityPair } from "./ItemIdQuantityPair.sol";
+import { ShipInventoryLib } from "../systems/ShipInventoryLib.sol";
+import { ShipInventoryData } from "../codegen/index.sol";
+import { SortedVectorUtil } from "../utils/SortedVectorUtil.sol";
+import { ShipInventoryUpdateUtil } from "../utils/ShipInventoryUpdateUtil.sol";
 
 library ShipDeductShipInventoryLogic {
   function verify(
@@ -11,14 +15,26 @@ library ShipDeductShipInventoryLogic {
     ItemIdQuantityPair[] memory items,
     ShipData memory shipData
   ) internal pure returns (ShipInventoryDeducted memory) {
-    // TODO ...
-    //return ShipInventoryDeducted(...);
+    return ShipInventoryDeducted({ id: id, items: items });
   }
 
   function mutate(
     ShipInventoryDeducted memory shipInventoryDeducted,
     ShipData memory shipData
-  ) internal pure returns (ShipData memory) {
-    // TODO ...
+  ) internal returns (ShipData memory) {
+    uint256 shipId = shipInventoryDeducted.id;
+    ItemIdQuantityPair[] memory itemsToDeduct = shipInventoryDeducted.items;
+    ShipInventoryData[] memory currentInventory = ShipInventoryLib.getAllInventory_(shipId);
+    ItemIdQuantityPair[] memory currentInventoryPairs = ShipInventoryUpdateUtil.convertToItemIdQuantityPairs(
+      currentInventory
+    );
+    ItemIdQuantityPair[] memory updatedInventory = SortedVectorUtil.subtractItemIdQuantityPairs(
+      currentInventoryPairs,
+      itemsToDeduct
+    );
+
+    ShipInventoryUpdateUtil.updateShipAllInventory(shipId, updatedInventory);
+
+    return shipData;
   }
 }
