2907fe315453ad00570e12ce0266d6e9811c0658
yangjiefeng <yangjiefeng@gmail.com>
2024-09-22 13:45:45 +0800

    update RosterPutInInventory logic

--- packages/contracts/src/systems/RosterPutInShipInventoryLogic.sol
+++ packages/contracts/src/systems/RosterPutInShipInventoryLogic.sol
@@ -2,10 +2,24 @@
 pragma solidity >=0.8.24;
 
 import { RosterShipInventoryPutIn } from "./RosterEvents.sol";
-import { RosterData } from "../codegen/index.sol";
+import { RosterData, PlayerData, Player, ShipData, Ship } from "../codegen/index.sol";
 import { ItemIdQuantityPair } from "./ItemIdQuantityPair.sol";
+import { RosterUtil } from "../utils/RosterUtil.sol";
+import { RosterStatus } from "./RosterStatus.sol";
+import { PlayerDelegationLib } from "./PlayerDelegationLib.sol";
+import { ShipDelegationLib } from "./ShipDelegationLib.sol";
+import { PlayerUtil } from "../utils/PlayerUtil.sol";
+import { RosterId } from "./RosterId.sol";
+import { RosterDataInstance } from "../utils/RosterDataInstance.sol";
 
 library RosterPutInShipInventoryLogic {
+  using RosterDataInstance for RosterData;
+
+  error SenderNotPlayerOwner();
+  error PlayerNotRosterOwner();
+  error RosterIsUnassignedShips();
+  error RosterNotCloseEnoughToTransfer();
+
   function verify(
     uint256 playerId,
     uint32 sequenceNumber,
@@ -14,15 +28,57 @@ library RosterPutInShipInventoryLogic {
     uint32 updatedCoordinatesX,
     uint32 updatedCoordinatesY,
     RosterData memory rosterData
-  ) internal pure returns (RosterShipInventoryPutIn memory) {
-    // TODO ...
-    //return RosterShipInventoryPutIn(...);
+  ) internal view returns (RosterShipInventoryPutIn memory) {
+    PlayerUtil.assertSenderIsPlayerOwner(playerId);
+    PlayerData memory player = Player.get(playerId);
+    RosterId memory rosterId = RosterId(playerId, sequenceNumber);
+    RosterUtil.assertPlayerIsRosterOwner(playerId, rosterId);
+    RosterUtil.assertRosterIsNotUnassignedShips(sequenceNumber);
+
+    if (rosterData.status == uint8(RosterStatus.UNDERWAY)) {
+      // TODO: Implement clock functionality
+      uint64 currentTimestamp = uint64(block.timestamp);
+
+      (bool updatable, uint64 coordinatesUpdatedAt, uint8 newStatus) = rosterData.isCurrentLocationUpdatable(
+        currentTimestamp,
+        updatedCoordinatesX,
+        updatedCoordinatesY
+      );
+
+      if (updatable) {
+        rosterData.updatedCoordinatesX = updatedCoordinatesX;
+        rosterData.updatedCoordinatesY = updatedCoordinatesY;
+        rosterData.coordinatesUpdatedAt = coordinatesUpdatedAt;
+        rosterData.status = newStatus;
+      }
+    }
+
+    rosterData.assertRosterIslandCloseEnoughToTransfer(player.claimedIslandX, player.claimedIslandY);
+
+    return
+      RosterShipInventoryPutIn({
+        playerId: playerId,
+        sequenceNumber: sequenceNumber,
+        shipId: shipId,
+        itemIdQuantityPairs: itemIdQuantityPairs,
+        updatedCoordinatesX: updatedCoordinatesX,
+        updatedCoordinatesY: updatedCoordinatesY
+      });
   }
 
   function mutate(
     RosterShipInventoryPutIn memory rosterShipInventoryPutIn,
     RosterData memory rosterData
-  ) internal pure returns (RosterData memory) {
-    // TODO ...
+  ) internal returns (RosterData memory) {
+    uint256 shipId = rosterShipInventoryPutIn.shipId;
+    ItemIdQuantityPair[] memory itemIdQuantityPairs = rosterShipInventoryPutIn.itemIdQuantityPairs;
+
+    // Deduct items from player inventory
+    PlayerDelegationLib.deductItems(rosterShipInventoryPutIn.playerId, itemIdQuantityPairs);
+
+    // Add items to ship inventory
+    ShipDelegationLib.increaseShipInventory(shipId, itemIdQuantityPairs);
+
+    return rosterData;
   }
 }
