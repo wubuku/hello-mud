b1cf12b35af37d438f1f9c96ec0f881fbf4bdba3
yangjiefeng <yangjiefeng@gmail.com>
2024-09-23 13:30:40 +0800

    update test script

---
+++ packages/contracts/script/GatherIslandResourcesTest.s.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import { Systems } from "@latticexyz/world/src/codegen/tables/Systems.sol";
+import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
+import { RESOURCE_SYSTEM } from "@latticexyz/world/src/worldResourceTypes.sol";
+
+import { IWorld } from "../src/codegen/world/IWorld.sol";
+import { Energy } from "../src/tokens/Energy.sol";
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { SkillType } from "../src/systems/SkillType.sol";
+import { PlayerIdGenerator, ShipIdGenerator, ShipBattleIdGenerator, RosterData, Roster } from "../src/codegen/index.sol";
+import { ItemIdQuantityPair } from "../src/systems/ItemIdQuantityPair.sol";
+import { RosterUtil } from "../src/utils/RosterUtil.sol";
+
+contract GatherIslandResourcesTest is Script {
+  //
+  // forge script script/GatherIslandResourcesTest.s.sol:GatherIslandResourcesTest --sig "run(address)" <WORLD_ADDRESS> --broadcast --rpc-url http://localhost:8545
+  //
+  function run(address worldAddress) external {
+    // Specify a store so that you can use tables directly in PostDeploy
+    StoreSwitch.setStoreAddress(worldAddress);
+
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    address deployerAddress = vm.addr(deployerPrivateKey);
+
+    // Start broadcasting transactions from the deployer account
+    vm.startBroadcast(deployerPrivateKey);
+
+    console.log("Current account:", deployerAddress);
+
+    uint256 balance = deployerAddress.balance;
+    console.log("Account balance:", balance);
+
+    // Energy energyToken = new Energy(deployerAddress);
+    // address energyTokenAddress = address(energyToken);
+    // console.log("ENERGY Token address:", energyTokenAddress);
+
+    // IERC20 energyIErc20 = IERC20(energyTokenAddress);
+    // energyIErc20.approve(systemAddress, 10000 * 10 ** 18);
+    // console.log("Approved AggregatorServiceSystem to spend ENERGY tokens");
+
+    IWorld world = IWorld(worldAddress);
+    // world.app__energyTokenCreate(energyTokenAddress);
+    // console.log("Set ENERGY token address for the world");
+
+    uint256 playerId = 1;
+
+    // uint32 unassignedShipsRosterSequenceNumber = 0;
+    // uint256 toRosterPlayerId = playerId;
+    // uint32 toRosterSequenceNumber = 1;
+
+    // uint256 playerShipId; // = ShipIdGenerator.get();
+    // RosterData memory rosterData = Roster.get(toRosterPlayerId, toRosterSequenceNumber);
+    // playerShipId = rosterData.shipIds[0];
+    // console.log("Player ship ID:", playerShipId);
+
+    // uint32 firstIslandX = 2147483647;
+    // uint32 firstIslandY = 2147483647;
+    // uint32 currentRosterSequenceNumber = 1;
+    // (uint32 originCoordinatesX, uint32 originCoordinatesY) = RosterUtil.getRosterOriginCoordinates(
+    //   firstIslandX,
+    //   firstIslandY,
+    //   currentRosterSequenceNumber
+    // );
+
+    world.app__playerGatherIslandResources(
+      playerId
+    );
+    console.log("Island resources gathered");
+
+    vm.stopBroadcast();
+  }
+}
