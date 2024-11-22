9ef70ad63566fc66ff36754b96620ccb24453c50
Li Yongchun <return@126.com>
2024-11-22 21:49:35 +0800

    使用batchcall批量添加钱包地址到白名单

---
+++ packages/contracts/script/BatchAddWLExecute.s.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+
+import { IWorld } from "../src/codegen/world/IWorld.sol";
+//import { Tasks, TasksData } from "../src/codegen/index.sol";
+
+// We need to create the ResourceID for the System we are calling
+import { ResourceId, WorldResourceIdLib, WorldResourceIdInstance } from "@latticexyz/world/src/WorldResourceId.sol";
+import { RESOURCE_SYSTEM } from "@latticexyz/world/src/worldResourceTypes.sol";
+
+import { SystemCallData } from "@latticexyz/world/src/modules/init/types.sol";
+
+// 添加到白名单 forge script BatchAddWL.s.sol:BatchCall --sig "run(address)" 0x63381030dda22c888f2548436c73146ef835ab9e --broadcast --rpc-url https://odyssey.storyrpc.io/
+// 从白名单移除：cast send 0xe271ed2e37a0926a70f824ca7dc1bc74d1d586e7 "app__islandClaimWhitelistUpdate(address,bool)" "0x20C22dC5022Aeabbd30c8b594bfd44fB167abE70" false --rpc-url "http://127.0.0.1:8545" --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --json
+// cast send {worldAddress} "app__islandClaimWhitelistUpdate(address,bool)" "walletAddress" false --rpc-url "http://127.0.0.1:8545" --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --json
+contract BatchCall is Script {
+  function run(address worldAddress) external {
+    StoreSwitch.setStoreAddress(worldAddress);
+    IWorld world = IWorld(worldAddress);
+
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    // Start broadcasting transactions from the deployer account
+    vm.startBroadcast(deployerPrivateKey);
+
+    ResourceId systemId = WorldResourceIdLib.encode({
+      typeId: RESOURCE_SYSTEM,
+      namespace: "app",
+      name: "IslandClaimWhite"
+    });
+    address[3] memory walletAddresses = [
+      0x269E2038cb7b084830f3c12c5041A5c52677d525,
+      0x79785B77EE18F14BcE7006d9583D26279A39bAF7,
+      0xc173bB17b5D2C7BCEd8a6f50E6F9c1bD6bde48DD
+    ];
+    SystemCallData[] memory calls = new SystemCallData[](walletAddresses.length);
+    for (uint i = 0; i < walletAddresses.length; i++) {
+      calls[i].systemId = systemId;
+      calls[i].callData = abi.encodeWithSignature("islandClaimWhitelistAdd(address)", walletAddresses[i]);
+    }
+    bytes[] memory returnData = IWorld(worldAddress).batchCall(calls);
+    for (uint i = 0; i < returnData.length; i++) {
+      console.log("The return value is:");
+      console.logBytes(returnData[i]);
+    }
+    //world.app__islandClaimWhitelistUpdate(accountAddress, allowed);
+    vm.stopBroadcast();
+  }
+}
---
+++ packages/contracts/script/CreateBatchAddWLExcute.ps1
@@ -0,0 +1,108 @@
+
+$worldAddress = "0x63381030dda22c888f2548436c73146ef835ab9e"
+$rpcUrl = "http://127.0.0.1:8545"
+
+$startLocation = $PSScriptRoot; 
+Set-Location $startLocation
+# 读取钱包地址
+$walletAddresses = $null
+# 检查 walletaddress.txt 是否存在
+$walletAddressesFile = "$startLocation\walletaddress.txt"
+if (-not (Test-Path -Path $walletAddressesFile -PathType Leaf)) {
+    "文件 $walletAddressesFile 不存在 " | Write-Host  -ForegroundColor Red
+    return
+} 
+else {
+    $walletAddresses = Get-Content -Path $walletAddressesFile
+}
+$resolvedAddresses = @()
+# 循环输出 $walletAddresses 中的值
+foreach ($address in $walletAddresses) {
+    if ($null -ne $address -and $address -ne "") {
+        if (-Not $resolvedAddresses.Contains($address)) {
+            Write-Host $address
+            $resolvedAddresses += $address
+        }
+    }
+}
+if ($resolvedAddresses.Count -lt 1) {
+    "请至少提供一个加到白名单的钱包地址。" | Write-Host  -ForegroundColor Red
+    return
+}
+
+$templateLineKeywords = "address[] memory walletAddresses"
+
+$templateFileContent = $null
+$templateFile = "$startLocation\BatchAddWLTemplate.s.sol"
+if (-not (Test-Path -Path $templateFile -PathType Leaf)) {
+    "文件 $templateFile 不存在 " | Write-Host  -ForegroundColor Red
+    return
+} 
+else {
+    $templateFileContent = Get-Content -Path $templateFile
+}
+$templateLine = $null
+foreach ($line in $templateFileContent) {
+    if ($line.Contains($templateLineKeywords)) {
+        $templateLine = $line
+        break;
+    }
+}
+if ($null -eq $templateLine) {
+    "没有找到含有关键字:$templateLineKeywords 的模板行" | Write-Host -ForegroundColor Red
+    return
+}
+
+$replaceTo = "address[$($resolvedAddresses.Count)] memory walletAddresses = ["
+for ($i = 0; $i -lt $resolvedAddresses.Count; $i++) {
+    $replaceTo += "address($($resolvedAddresses[$i]))"
+    if ($i -lt $resolvedAddresses.Count - 1) {
+        $replaceTo += ","
+    }
+}
+$replaceTo += "];"
+
+$replaceTo | Write-Host -ForegroundColor Green
+
+
+$pathBatchAddWL = $startLocation + "\BatchAddWL"
+if (-Not (Test-Path $pathBatchAddWL)) {
+    # 如果不存在，则创建目录
+    New-Item -ItemType Directory -Path $pathBatchAddWL
+    "创建目录: $pathBatchAddWL" | Write-Host 
+} 
+$now = Get-Date
+$formattedNow = $now.ToString('yyyyMMddHHmmss')
+$batchAddWLExecuteFile = "$startLocation\BatchAddWLExecute_$formattedNow" + ".s.sol"
+"生成Powershell脚本:$batchAddWLExecuteFile" | Write-Host -ForegroundColor Blue
+$logFile = "$pathBatchAddWL\BatchAddWLExecute_$formattedNow" + ".log"
+
+foreach ($line in $templateFileContent) {
+    if ($line.Contains($templateLineKeywords)) {        
+        $replaceTo | Tee-Object -FilePath $batchAddWLExecuteFile -Append  
+        $replaceTo | Tee-Object -FilePath $logFile -Append 
+    }
+    # elseif ($line.Contains("import") -and $line.Contains("..")) {
+    #     $import = $line.Replace("..", "../.."); 
+    #     $import | Tee-Object -FilePath $batchAddWLExecuteFile -Append  
+    # }
+    else {
+        
+        $line | Tee-Object -FilePath $batchAddWLExecuteFile -Append  
+    }
+}
+Write-Host "文件已保存为 $batchAddWLExecuteFile"
+
+Write-Host "现在开始执行 $batchAddWLExecuteFile"
+
+try {
+    $command = "forge script $batchAddWLExecuteFile" + ":BatchCall --sig ""run(address)"" $worldAddress --broadcast --rpc-url $rpcUrl"
+    $command | Tee-Object -FilePath $logFile -Append | Write-Host  -ForegroundColor Blue
+    #$executeResult = Invoke-Expression -Command $command
+    Invoke-Expression -Command $command | Tee-Object -FilePath $logFile -Append 
+}
+catch {    
+    "$($_.Exception.Message) `n" | Tee-Object -FilePath $logFile -Append | Write-Host -ForegroundColor Red
+}
+"查看日志:$logFile" | Write-Host -ForegroundColor Blue
+
