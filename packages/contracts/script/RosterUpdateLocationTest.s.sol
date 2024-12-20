35ee26dcd3294e3bcc62274b18b88fa9ce57911b
whilliy <return@126.com>
2024-12-20 23:50:53 +0800

    增加中间点的显示。

--- packages/contracts/script/RosterUpdateLocationTest.s.sol
+++ packages/contracts/script/RosterUpdateLocationTest.s.sol
@@ -14,16 +14,18 @@ import { Energy } from "../src/tokens/Energy.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { SkillType } from "../src/systems/SkillType.sol";
-import { PlayerIdGenerator, ShipIdGenerator, ShipBattleIdGenerator, RosterData, Roster } from "../src/codegen/index.sol";
+import { SailIntPointData, PlayerIdGenerator, ShipIdGenerator, ShipBattleIdGenerator, RosterData, Roster } from "../src/codegen/index.sol";
 import { ItemIdQuantityPair } from "../src/systems/ItemIdQuantityPair.sol";
 import { RosterUtil } from "../src/utils/RosterUtil.sol";
 import { SpeedUtil } from "../src/utils/SpeedUtil.sol";
 import { Coordinates } from "../src/systems/Coordinates.sol";
 import { UpdateLocationParams } from "../src/systems/UpdateLocationParams.sol";
 
+import { SailIntPointLib } from "../src/systems/SailIntPointLib.sol";
+
 contract RosterUpdateLocationTest is Script {
   //
-  // forge script script/RosterUpdateLocationTest.s.sol:RosterUpdateLocationTest --sig "run(address)" <WORLD_ADDRESS> --broadcast --rpc-url http://localhost:8545
+  // forge script RosterUpdateLocationTest.s.sol:RosterUpdateLocationTest --sig "run(address)" 0x776086899eab4ee3953b7c037b2c0a13c7a1deed --broadcast --rpc-url https://odyssey.storyrpc.io/
   //
   function run(address worldAddress) external {
     // Specify a store so that you can use tables directly in PostDeploy
@@ -60,6 +62,23 @@ contract RosterUpdateLocationTest is Script {
     console.log("Roster origin coordinates:", rosterData.originCoordinatesX, rosterData.originCoordinatesY);
     console.log("Roster set sail at:", rosterData.setSailAt);
     console.log("Roster sail duration:", rosterData.sailDuration);
+    SailIntPointData[] memory sailIntermediatePoints = SailIntPointLib.getAllSailIntermediatePoints(
+      playerId,
+      currentRosterSequenceNumber
+    );
+    if (sailIntermediatePoints.length > 0) {
+      console.log("The roster has %d IntermediatePoints", sailIntermediatePoints.length);
+      for (uint i = 0; i < sailIntermediatePoints.length; i++) {
+        console.log(
+          "coordinatesX:%d,coordinatesY:%d,SegmentShouldStartAt:%d",
+          sailIntermediatePoints[i].coordinatesX,
+          sailIntermediatePoints[i].coordinatesY,
+          sailIntermediatePoints[i].segmentShouldStartAt
+        );
+      }
+    } else {
+      console.log("The roster has not IntermediatePoints");
+    }
 
     uint64 elapsedTime = uint64(currentTimestamp - rosterData.setSailAt);
     console.log("Elapsed sail time:", elapsedTime);
@@ -87,11 +106,7 @@ contract RosterUpdateLocationTest is Script {
     updateLocationParams.updatedSailSegment = currentSegment;
     updateLocationParams.updatedAt = uint64(currentTimestamp);
 
-    world.app__uniApiRosterUpdateLocation(
-      playerId,
-      currentRosterSequenceNumber,
-      updateLocationParams
-    );
+    world.app__uniApiRosterUpdateLocation(playerId, currentRosterSequenceNumber, updateLocationParams);
     console.log("Roster updated location");
 
     vm.stopBroadcast();
